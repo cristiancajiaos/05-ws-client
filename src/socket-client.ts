d95c53a5f7e22fd96cb973033ac56d012b2d2564
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-03 14:38:13 -0400

    Se muestra si hay un cliente conectado o desconectado

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,4 +1,4 @@
-import { Manager } from 'socket.io-client';
+import { Manager, Socket } from 'socket.io-client';
 export const connectToServer = () => {
 
   // http://localhost:3000/socket.io/socket.io.js
@@ -6,4 +6,18 @@ export const connectToServer = () => {
 
   const socket = manager.socket('/');
   console.log(socket);
+
+  addListeners(socket);
+}
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status");
+
+  socket.on('connect', () => {
+    serverStatusLabel!.innerHTML = 'connected';
+  })
+
+  socket.on('disconnect', () => {
+    serverStatusLabel!.innerHTML = 'disconnected';
+  })
 }
