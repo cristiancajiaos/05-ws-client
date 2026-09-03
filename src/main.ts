5c2766bdede12c8ab01fbc5837c5d797337d0191
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-03 15:58:05 -0400

    Se agrega botón e input para enviar JSON Web Token

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { connectToServer } from './socket-client.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Websockets - Client</h1>
+
+    <input id="jwt-token" placeholder="JSON Web Token"/>
+    <button id="btn-connect">Connect</button>
+
+    <br/>
     <span id="server-status">Offline</span>
 
     <ul id="clients-ul">
@@ -21,4 +26,17 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 //setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-connectToServer();
+// connectToServer();
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if (jwtToken.value.trim().length <= 0) {
+    alert('Enter a valid JWT');
+    return;
+  }
+  connectToServer(jwtToken.value.trim());
+});
+
